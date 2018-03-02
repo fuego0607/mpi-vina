343644927599fb7e46dcbcb849c173dd0cb8159b
Jeremy Hofer <fuego0607@gmail.com>
2018-03-02 18:51:10 -0500

    testing initial python impementation

--- py_src/src/test.py
+++ py_src/src/test.py
@@ -5,3 +5,15 @@ if MPI.COMM_WORLD.rank == 0:
 else:
         a = MPI.COMM_WORLD.recv(source = 0)
         print a
+
+        
+from mpi4py import MPI
+import sys
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+name = MPI.Get_processor_name()
+
+sys.stdout.write(
+    "Hello, World! I am process %d of %d on %s.\n"
+    % (rank, size, name))
