0c16b80479fcf94bfbd504acddfec17fd0c9cccf
Abhay Aradhya <abhayaradhya@abhays-mbp.widener.edu>
2018-03-16 13:25:31 -0400

    Added more functionality to score skeleton. Still need to specify output directories.

--- py_src/src/score.py
+++ py_src/src/score.py
@@ -1,58 +1,29 @@
 from subprocess import call
 import os
 
-def runNNScore(receptors_pdbqt, vinaScores_pdbqt):
-	for index, score in enumerate(vinaScores_pdbqt):
-		call(["python", "NNScore.py", "-receptor", receptors_pdbqt[index], "-ligand", score, "-vina_executable", "/Vina/vina"])
+def runVina(vinaConf, ligand):
+	call(["vina", "--config", vinaConf, "--ligand", ligand, "--out", conf[vinaOutput], ">", "${stdout} 2> ${stderr}"])
 
-def runDSXScore(receptors_pdb, vinaScores_mol2):
-	#build dsx_output_directory for result output
+def runNNScore(receptor_pdbqt, vinaScore_pdbqt, conf):
+	#specify output
+	#specif neural networks directory
+	call(["python", "NNScore.py", "-receptor", receptor_pdbqt, "-ligand", vinaScore_pdbqt, "-networks_dir", "./networks/top_3_networks/"])
+
+def runDSXScore(receptor_pdb, vinaScore_mol2, conf):
+	#specify output
 	potentials = "/pdb_pot_0511/"
-
-	for index, score in enumerate(vinaScores_mol2):
-		call(["dsx_linux_64.lnx", "-P", receptors_pdb[index], "-L", score, "-D", potentials, "-F", "dsx_output_directory"])
+	call(["dsx_linux_64.lnx", "-P", receptor_pdb, "-L", vinaScore_mol2, "-D", potentials, "-F", conf["output"]])
 
 def runRFScore():
 	#callRFScore
 
-def runXScore(receptors_pdb, vinaScores_mol2):
+def runXScore(receptor_pdb, vinaScore_mol2, conf, input_file):
 	#convert receptor from .pdb to .pdb_fixed using xscore
-	for receptor in receptors_pdb:
-		call(["xscore", "-fixpdb", receptor, "pdb_fix_directory"])
-	
+	call(["xscore", "-fixpdb", receptor, "pdb_fix_directory"])
 	#convert vina score from .mol2 to .mol2_fixed using xscore
-	for score in vinaScores_mol2:
-		call(["xscore", "-fixmol2", score, "mol2_fix_directory"])
-	
-	#converted_score = ** get list of all mol2_fixed scores in mol2_fix_directory **
-	#for converted_score in converted_scores:
-		#create score specific config file using base config file template and path names
-		#https://github.com/elmotec/massedit seems like a good replacement for sed
-		#run xscore with input file
-		#call(["xscore", input_file])
+	call(["xscore", "-fixmol2", score, "mol2_fix_directory"])
+	call(["xscore", input_file])
 
 def convertToMol2(vinaScores_pdbqt):
 	#need to define mol2 directory
-	for score in vinaScores_pdbqt:
-		call(["obabel", "-i", "pdbqt", score, "-o", "mol2", "-O", "mol2_directory", "-h"])
-
-
-if __name__ == '__main__':
-	#get receptor pdbqt dir, need to change this to a pdbqt-specific receptor directory
-	receptors_pdbqt = os.listdir("/receptors")
-	#get receptor pdb dir, need to establish a receptor pdb file
-	#get vina scores pdbqt dir
-	vinaScores_pdbqt = os.listdir("/ligands")
-	convertToMol2(vinaScores_pdbqt)
-	#vinaScores_mol2 = os.listdir("/ligands_mol2")  *** need to establish ligands mol2 directory
-
-	#runNNScore(receptors_pdbqt, vinaScores_pdbqt)
-	#runDSXScore(receptors_pdb, vinaScores_mol2)
-	#runRFScore
-	#runXScore(receptors_pdb, vinaScores_mol2)
-
-	#all scoring methods assume receptors and lignd/vinaScore lists are built such that
-	#the elements at the same index correspond
-	#THIS IS A WRONG ASSUMPTION
-	#For loops inside scoring functions need to be adjusted
-	#need to map each ligand/vinaScore to its receptor
+	call(["obabel", "-i", "pdbqt", score, "-o", "mol2", "-O", "mol2_directory", "-h"])
